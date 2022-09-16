68f60fb7bfc181d788279ad552f3ee2bd7334d4c
Felipe Repker <102967559+feliperepker@users.noreply.github.com>
2022-09-16 16:08:16 -0300

    Adicionando backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,100 @@
 import express, { response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/conver-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
+import cors from 'cors'
+
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.get('/games', (request, response ) => { 
-    return response.json([]);
+const prisma = new PrismaClient({
+    log: ['query']
 })
 
-app.post('/ads', (request, response ) => { 
-    return response.json([]);
+
+
+app.get('/games', async (request, response ) => { 
+    const games = await prisma.game.findMany({
+        include:{
+            _count:{
+                select:{
+                    ads: true,
+                }
+            }
+        }
+    })
+    
+    return response.json(games);
 })
 
-app.get('/game/:id/ads', (request, response) =>{
+app.post('/games/:id/ads', async (request, response ) => { 
     const gameId = request.params.id;
-    return response.json([
-        {id: 1, name: 'Anuncio 1'},
-        {id: 2, name: 'Anuncio 2'},
-        {id: 3, name: 'Anuncio 3'},
-    ])
+
+    const body : any = request.body;
+
+    const ad = await prisma.ad.create({
+        data:{
+            id: '',
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourStringToMinutes(body.hourStart),
+            hourEnd: convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+    return response.json([]);
 })
 
-app.get('/ads/:id/discord', (request, response) =>{
+app.get('/games/:id/ads', async (request, response) =>{
+    const gameId = request.params.id;
+
+    const ads = await prisma.ad.findMany({
+        select:{
+            id:true,
+            name: true,
+            weekDays:true,
+            useVoiceChannel:true,
+            yearsPlaying:true,
+            hourStart:true,
+            hourEnd:true
+        },
+        where:{
+            gameId,
+        },
+        orderBy:{
+            createdAt:'desc',
+        }
+    })
+    return response.json(ads.map(ad =>{
+        return{
+            ...ad,
+            weekDays:ad.weekDays.split(','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd),
+        }
+    }))
+})
+
+app.get('/ads/:id/discord', async (request, response) =>{
     const adId = request.params.id;
-    return response.json([])
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select:{
+            discord:true,
+        },
+        where:{
+            id: adId
+        }
+
+    })
+
+    return response.json({
+        discor: ad.discord,
+    })
 })
 
 app.listen(3333)
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,73 +1,115 @@
 import './styles/main.css';
 import logoImg from './assets/logo-esports.svg';
-import { MagnifyingGlassPlus } from 'phosphor-react';
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
+import { useState, useEffect} from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { GameController } from 'phosphor-react';
+import { Input } from './components/Form/input';
+
+interface Game{
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+    .then(response => response.json())
+    .then(data =>{
+      setGames(data)
+    })
+  }, [])
 
   return (
-<div className='max-w-[1344] mx-auto flex flex-col items-center my-20'>
+<div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
     <img src={logoImg}/>
     <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.</h1>
     
     <div className='grid grid-cols-6 gap-6 mt-16'>
-      <a href="" className='relative rounded-lg overflow-hidden'>
-        <img src="./image1.png" alt="" />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>League of Legends</strong>
-          <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden'>
-        <img src="./image2.png" alt="" />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>League of Legends</strong>
-          <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden'>
-        <img src="./image3.png" alt="" />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>League of Legends</strong>
-          <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden'>
-        <img src="./image4.png" alt="" />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>League of Legends</strong>
-          <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden'>
-        <img src="./image5.png" alt="" />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>League of Legends</strong>
-          <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-        </div>
-      </a>
-      <a href="" className='relative rounded-lg overflow-hidden'>
-        <img src="./image6.png" alt="" />
-        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-          <strong className='font-bold text-white block'>League of Legends</strong>
-          <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-        </div>
-      </a>
-    </div>
+     {games.map(game =>{
+      return(
+        <GameBanner 
+          key={game.id} bannerUrl={game.bannerUrl} title={game.title} adsCount={game._count.ads}
+        />
+      )
+     })}
+     
+     
+  </div>
 
-    <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg  mt-8 overflow-hidden'>
-      <div className='bg-[#2A2634] px-8 py-6 flex justify-between'>
-        <div>
-          <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-          <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
-        </div>
-        
-        <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded-lg flex items-center gap-3'>
-          <MagnifyingGlassPlus size={24}/>Publicar anúncio
-          </button>
-      </div>
+    <Dialog.Root>
+      <CreateAdBanner/>
       
+      <Dialog.Portal>
+        <Dialog.Overlay className='bg-black/60 inset-0 fixed'>
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+            <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
+              <form className='mt-8 flex flex-col gap-4'>
+                  <div className='flex flex-col gap-2'>
+                    <label htmlFor='game' className='font-semibold'>Qual o game?</label>
+                   <Input id='game' placeholder='Selecione o game que deseja'/>
+                  </div>
 
-    </div>
+                  <div className='flex flex-col gap-2'>
+                    <label htmlFor='name'>Seu nome (ou nickname)</label>
+                    <Input id='name' placeholder='Como te chamam dentro do game?' />
+                  </div>
+                  <div className='grid grid-cols-2 gap-6'>
+                    <div className='flex flex-col gap-2'>
+                      <label htmlFor="yearPlaying">Joga há quantos anos?</label>
+                      <Input id='yearsPlaying' type="number" placeholder='Tudo bem ser ZERO' />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                      <label htmlFor='discord'>Qual seu discord?</label>
+                      <Input id='discord =' type='text' placeholder='Usuario#0000'></Input>
+                    </div>
+                  </div>
+
+                  <div className='flex gap-6'>
+                    <div className='flex flex-col gap-2'>
+                      <label htmlFor="weekDays">Quando costuma jogar?</label>
+                      <div className='grid grid-cols-4 gap-2 '>
+                        <button className='w-8 h-8 rounded bg-zinc-900' title='Domingo'>D</button>
+                        <button className='w-8 h-8 rounded bg-zinc-900' title='Segunda'>S</button>
+                        <button className='w-8 h-8 rounded bg-zinc-900' title='Terça'>T</button>
+                        <button className='w-8 h-8 rounded bg-zinc-900' title='Quarta'>Q</button>
+                        <button className='w-8 h-8 rounded bg-zinc-900' title='Quinta'>Q</button>
+                        <button className='w-8 h-8 rounded bg-zinc-900' title='Sexta'>S</button>
+                        <button className='w-8 h-8 rounded bg-zinc-900' title='Sábado'>S</button>
+                      </div>
+                    </div>
+                    <div className='flex flex-col gap-2 flex-1'>
+                      <label htmlFor='hourStart'>Qual horário do dia?</label>  
+                      <div className='grid grid-cols-2 gap-2'>
+                        <Input id='hourStart' type="time" placeholder='De' />
+                        <Input id='hourEnd' type="time" placeholder='Até' />
+                      </div>
+                    </div> 
+                  </div>
+                  <div>
+                    <Input type="checkbox" />
+                    Costumo me conectar ao chat de voz
+                  </div>
+                  <footer>
+                    <button>Cancelar</button>
+                    <button type='submit'>
+                      <GameController/>
+                      Encontrar duo
+                    </button>
+                  </footer>
+              </form>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root> 
 </div>
   )
 }
